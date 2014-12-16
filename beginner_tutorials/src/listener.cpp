00cfe224e73a6fdb0bea6d6d7beeafdb54ce8370
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-12-16 22:03:40 +0100

    Added working keyboard

--- beginner_tutorials/src/listener.cpp
+++ beginner_tutorials/src/listener.cpp
@@ -3,18 +3,23 @@
 
 void chatterCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
-  ROS_INFO("linear x: [%f]", msg->linear.x);
-  ROS_INFO("linear y: [%f]", msg->linear.y);
-  ROS_INFO("linear z: [%f]", msg->linear.z);
+	ROS_INFO("linear x: [%f]", msg->linear.x);
+	ROS_INFO("linear y: [%f]", msg->linear.y);
+	ROS_INFO("linear z: [%f]", msg->linear.z);
+
+	ROS_INFO("angular x: [%f]", msg->angular.x);
+	ROS_INFO("angular y: [%f]", msg->angular.y);
+	ROS_INFO("angular z: [%f]", msg->angular.z);
+	ROS_INFO("-------------------------");
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "listener");
-  ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("cmd_vel", 1000, chatterCallback);
+	ros::init(argc, argv, "listener");
+	ros::NodeHandle n;
+	ros::Subscriber sub = n.subscribe("cmd_vel", 1000, chatterCallback);
 
-  ros::spin();
+	ros::spin();
 
-  return 0;
+	return 0;
 }
--- beginner_tutorials/src/talker.cpp
+++ beginner_tutorials/src/talker.cpp
@@ -1,33 +1,92 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
 #include <sstream>
+#include <stdio.h>
+#include <termios.h>
+
+#define KEYCODE_R 0x43
+#define KEYCODE_L 0x44
+#define KEYCODE_U 0x41
+#define KEYCODE_D 0x42
+#define KEYCODE_Q 0x71
+
+int kfd = 0;
+struct termios cooked, raw;
+double linear_, angular_, l_scale_, a_scale_;
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "talker");
-  ros::NodeHandle n;
-  ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
-  
-  ros::Rate loop_rate(10);
+	ros::init(argc, argv, "talker");
+	ros::NodeHandle n;
+	ros::Publisher twist_pub_ = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
 
-  int count = 0;
-  while (ros::ok())
-  {
-    geometry_msgs::Twist twist;
-    twist.linear.x = 1.0;
-    twist.linear.y = 1.0;
-    twist.linear.z = 1.0;
-    twist.angular.x = 1.0;
-    twist.angular.z = 1.0;
+	ros::Rate loop_rate(10);
 
-    cmd_vel_pub.publish(twist);
+	char c;
+	bool dirty = false;
+	geometry_msgs::Twist twist;
+	a_scale_ = l_scale_ = 2;
 
-    ros::spinOnce();
+	// get the console in raw mode
+	tcgetattr(kfd, &cooked);
+	memcpy(&raw, &cooked, sizeof(struct termios));
+	raw.c_lflag &=~ (ICANON | ECHO);
+	// Setting a new line, then end of file
+	raw.c_cc[VEOL] = 1;
+	raw.c_cc[VEOF] = 2;
+	tcsetattr(kfd, TCSANOW, &raw);
 
-    loop_rate.sleep();
-    ++count;
-  }
+	puts("Reading from keyboard");
+	puts("---------------------------");
+	puts("Use arrow keys to move the turtle.");
 
-  return 0;
+	while (ros::ok())
+	{
+		// get the next event from the keyboard
+		if(read(kfd, &c, 1) < 0)
+		{
+			perror("read():");
+			exit(-1);
+		}
+
+		linear_ = angular_ = 0;
+		ROS_DEBUG("value: 0x%02X\n", c);
+
+		switch(c)
+		{
+			case KEYCODE_L:
+				ROS_DEBUG("LEFT");
+				angular_ = 1.0;
+				dirty = true;
+				break;
+			case KEYCODE_R:
+				ROS_DEBUG("RIGHT");
+				angular_ = -1.0;
+				dirty = true;
+				break;
+			case KEYCODE_U:
+				ROS_DEBUG("UP");
+				linear_ = 1.0;
+				dirty = true;
+				break;
+			case KEYCODE_D:
+				ROS_DEBUG("DOWN");
+				linear_ = -1.0;
+				dirty = true;
+				break;
+		}
+
+		twist.angular.z += a_scale_ * angular_;
+		twist.linear.x += l_scale_ * linear_;
+		geometry_msgs::Twist msg = twist;
+
+		if(dirty == true)
+		{
+			twist_pub_.publish(msg);
+			dirty = false;
+		}
+	}
+
+	return 0;
 }
 
