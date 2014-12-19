9792c97c9906dfd2944ec248ca54602cb5abdd09
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-12-19 21:01:53 +0100

    This should probably work...

--- robo/robo.ino
+++ robo/robo.ino
@@ -5,21 +5,25 @@
 #define LEFTREV   7
 #define LEFTEN	 24
 #define LEFTFWD   6
-
 #define RIGHTREV  3
 #define RIGHTEN  25
 #define RIGHTFWD  2
+
+class NewHardware : public ArduinoHardware {
+public:
+  NewHardware() : ArduinoHardware(&Serial1, 57600) {};
+};
+
+Motor m1(LEFTREV, LEFTEN, LEFTFWD);
+Motor m2(RIGHTREV, RIGHTEN, RIGHTFWD);
+
+ros::NodeHandle_<NewHardware> nh;
+ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", cmd_vel_cb);
 
 void cmd_vel_cb(const geometry_msgs::Twist& cmd_vel_msg)
 {
   // DO SOMETHING HERE
 }
-
-ros::NodeHandle nh;
-ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", cmd_vel_cb);
-
-Motor m1(LEFTREV, LEFTEN, LEFTFWD);
-Motor m2(RIGHTREV, RIGHTEN, RIGHTFWD);
 
 void setup()
 {
