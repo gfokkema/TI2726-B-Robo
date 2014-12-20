31683f7d5865e43f6ec0faf4ee667ba7fe04921d
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-12-21 00:59:54 +0100

    Added some sliders to customize behaviour

--- linefollower/src/analyzer.cpp
+++ linefollower/src/analyzer.cpp
@@ -7,13 +7,24 @@
 static const std::string OPENCV_WINDOW = "Image window";
 
 Analyzer::Analyzer()
-: it_(nh_)
+: it_(nh_),
+  canny_kernel(3), canny_ratio(4),
+  canny_min(30), canny_max(100),
+  gaussian_min(3), gaussian_max(15),
+  hough_min(20), hough_max(100),
+  hough_line_min(20), hough_line_max(100),
+  hough_gap_min(10), hough_gap_max(50)
 {
   // Subscrive to input video feed and publish output video feed
   image_transport::TransportHints hints("compressed", ros::TransportHints());
   image_sub_ = it_.subscribe("/camera/image", 1, &Analyzer::imageCb, this, hints);
 
   cv::namedWindow(OPENCV_WINDOW);
+  cv::createTrackbar("Gaussian: ", OPENCV_WINDOW, &gaussian_min, gaussian_max);
+  cv::createTrackbar("Canny edge min: ", OPENCV_WINDOW, &canny_min, canny_max);
+  cv::createTrackbar("Hough min: ", OPENCV_WINDOW, &hough_min, hough_max);
+  cv::createTrackbar("Hough line min: ", OPENCV_WINDOW, &hough_line_min, hough_line_max);
+  cv::createTrackbar("Hough gap min: ", OPENCV_WINDOW, &hough_gap_min, hough_gap_max);
 }
 
 Analyzer::~Analyzer()
@@ -32,15 +43,23 @@ Analyzer::imageCb(const sensor_msgs::ImageConstPtr& msg)
     return;
   }
 
-  cv::Mat out1;
   cv::Mat gray_out;
   cv::Mat canny_out;
-  cv::Mat gray_out1;
-  cv::Mat img1;
+  cv::Mat cdst;
+  std::vector<cv::Vec4i> lines;
+
   cv::cvtColor(cv_ptr->image, gray_out, CV_BGR2GRAY);
-  cv::GaussianBlur(gray_out, gray_out, cv::Size(9, 9), 0, 0);
-  cv::Canny(gray_out, canny_out, 25, 125, 3);
-  cv::imshow(OPENCV_WINDOW, canny_out);
+  cv::GaussianBlur(gray_out, gray_out, cv::Size(gaussian_min, gaussian_min), 0, 0);
+  cv::Canny(gray_out, canny_out, canny_min, canny_min * canny_ratio, canny_kernel);
+  cv::HoughLinesP(canny_out, lines, 1, CV_PI / 180, 50, 50, 10);
+
+  cv::cvtColor(canny_out, cdst, CV_GRAY2BGR);
+  for (size_t i = 0; i < lines.size(); i++) {
+    cv::Vec4i l = lines[i];
+    cv::line(cdst, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,0,255), 3);
+  }
+
+  cv::imshow(OPENCV_WINDOW, cdst);
   cv::waitKey(3);
 }
 
--- linefollower/src/analyzer.h
+++ linefollower/src/analyzer.h
@@ -10,6 +10,14 @@ public:
 
   void imageCb(const sensor_msgs::ImageConstPtr& msg);
 private:
+  int canny_ratio;
+  int canny_kernel;
+  int canny_min, canny_max;
+  int gaussian_min, gaussian_max;
+  int hough_min, hough_max;
+  int hough_line_min, hough_line_max;
+  int hough_gap_min, hough_gap_max;
+
   ros::NodeHandle nh_;
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
