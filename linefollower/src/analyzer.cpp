288bdd8b816025f3285ca8a28cd25b6305384d4a
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-12-21 01:20:13 +0100

    Add sensible defaults and add a utility window

--- linefollower/src/analyzer.cpp
+++ linefollower/src/analyzer.cpp
@@ -5,26 +5,28 @@
 #include <opencv2/highgui/highgui.hpp>
 
 static const std::string OPENCV_WINDOW = "Image window";
+static const std::string OPENCV_UTIL = "Util window";
 
 Analyzer::Analyzer()
 : it_(nh_),
   canny_kernel(3), canny_ratio(4),
-  canny_min(30), canny_max(100),
-  gaussian_min(3), gaussian_max(15),
-  hough_min(20), hough_max(100),
-  hough_line_min(20), hough_line_max(100),
-  hough_gap_min(10), hough_gap_max(50)
+  canny_min(50), canny_max(100),
+  gaussian_min(3), gaussian_max(9),
+  hough_min(50), hough_max(100),
+  hough_line_min(50), hough_line_max(100),
+  hough_gap_min(10), hough_gap_max(20)
 {
   // Subscrive to input video feed and publish output video feed
   image_transport::TransportHints hints("compressed", ros::TransportHints());
   image_sub_ = it_.subscribe("/camera/image", 1, &Analyzer::imageCb, this, hints);
 
   cv::namedWindow(OPENCV_WINDOW);
-  cv::createTrackbar("Gaussian: ", OPENCV_WINDOW, &gaussian_min, gaussian_max);
-  cv::createTrackbar("Canny edge min: ", OPENCV_WINDOW, &canny_min, canny_max);
-  cv::createTrackbar("Hough min: ", OPENCV_WINDOW, &hough_min, hough_max);
-  cv::createTrackbar("Hough line min: ", OPENCV_WINDOW, &hough_line_min, hough_line_max);
-  cv::createTrackbar("Hough gap min: ", OPENCV_WINDOW, &hough_gap_min, hough_gap_max);
+  cv::namedWindow(OPENCV_UTIL);
+  cv::createTrackbar("Gaussian: ",       OPENCV_UTIL, &gaussian_min, gaussian_max);
+  cv::createTrackbar("Canny edge min: ", OPENCV_UTIL, &canny_min, canny_max);
+  cv::createTrackbar("Hough min: ",      OPENCV_UTIL, &hough_min, hough_max);
+  cv::createTrackbar("Hough line: ",     OPENCV_UTIL, &hough_line_min, hough_line_max);
+  cv::createTrackbar("Hough gap: ",      OPENCV_UTIL, &hough_gap_min, hough_gap_max);
 }
 
 Analyzer::~Analyzer()
@@ -51,7 +53,7 @@ Analyzer::imageCb(const sensor_msgs::ImageConstPtr& msg)
   cv::cvtColor(cv_ptr->image, gray_out, CV_BGR2GRAY);
   cv::GaussianBlur(gray_out, gray_out, cv::Size(gaussian_min, gaussian_min), 0, 0);
   cv::Canny(gray_out, canny_out, canny_min, canny_min * canny_ratio, canny_kernel);
-  cv::HoughLinesP(canny_out, lines, 1, CV_PI / 180, 50, 50, 10);
+  cv::HoughLinesP(canny_out, lines, 1, CV_PI / 180, hough_min, hough_line_min, hough_gap_min);
 
   cv::cvtColor(canny_out, cdst, CV_GRAY2BGR);
   for (size_t i = 0; i < lines.size(); i++) {
