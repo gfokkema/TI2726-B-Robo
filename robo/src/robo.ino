a848e43cdb980866016603db326f2f0dfd0703f6
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2015-01-07 14:29:02 +0100

    Skeleton for proximity sensor

--- robo/src/robo.ino
+++ robo/src/robo.ino
@@ -20,7 +20,10 @@ Motor m2(RIGHTREV, RIGHTEN, RIGHTFWD);
 DualMotor motor(&m1, &m2);
 
 ros::NodeHandle_<NewHardware> nh;
-ros::Subscriber<geometry_msgs::Point32> sub("cmd_vel", cmd_vel_cb);
+ros::Subscriber<geometry_msgs::Point32> cmd_vel_sub("cmd_vel", cmd_vel_cb);
+
+long pulse_start = 0, pulse_end = 0;
+bool pulse_dirty = false;
 
 void cmd_vel_cb(const geometry_msgs::Point32& cmd_vel_msg)
 {
@@ -32,13 +35,30 @@ void cmd_vel_cb(const geometry_msgs::Point32& cmd_vel_msg)
 	motor.update(cmd_vel_msg.x, cmd_vel_msg.z);
 }
 
+void pulse_start_cb()
+{
+	pulse_start = micros();
+}
+
+void pulse_end_cb()
+{
+	pulse_end = micros();
+	pulse_dirty = true;
+}
+
 void setup()
 {
 	nh.initNode();
-	nh.subscribe(sub);
+	nh.subscribe(cmd_vel_sub);
 }
 
 void loop()
 {
+	if (pulse_dirty)
+	{
+		long pulse_dt = pulse_end - pulse_start;
+		// TODO: calculate distance here
+		int pulse_distance = 20;
+	}
 	nh.spinOnce();
 }
