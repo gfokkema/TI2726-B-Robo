0d612a4f4acd47e470227d1acea678f6e865592f
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2015-01-10 21:13:33 +0100

    Also split off displaying

--- linefollower/src/analyzer.cpp
+++ linefollower/src/analyzer.cpp
@@ -15,20 +15,16 @@ Analyzer::Analyzer() :
 					hough_gap_min(50), hough_gap_max(200) {
 	// Subscribe to input video feed on /camera/image
 	image_transport::TransportHints hints("compressed", ros::TransportHints());
-	image_sub_ = it_.subscribe("/camera/image", 1, &Analyzer::imageCb, this,
-			hints);
+	image_sub_ = it_.subscribe("/camera/image", 1, &Analyzer::imageCb, this, hints);
 
 	// Open a window for Hough detection
 	cv::namedWindow(OPENCV_HOUGH);
 	// Open a window for the Hough toolbar
 	cv::namedWindow(OPENCV_HOUGH_UTIL);
-	cv::createTrackbar("Canny edge min: ", OPENCV_HOUGH_UTIL, &canny_min,
-			canny_max);
+	cv::createTrackbar("Canny edge min: ", OPENCV_HOUGH_UTIL, &canny_min, canny_max);
 	cv::createTrackbar("Hough min: ", OPENCV_HOUGH_UTIL, &hough_min, hough_max);
-	cv::createTrackbar("Hough line: ", OPENCV_HOUGH_UTIL, &hough_line_min,
-			hough_line_max);
-	cv::createTrackbar("Hough gap: ", OPENCV_HOUGH_UTIL, &hough_gap_min,
-			hough_gap_max);
+	cv::createTrackbar("Hough line: ", OPENCV_HOUGH_UTIL, &hough_line_min, hough_line_max);
+	cv::createTrackbar("Hough gap: ", OPENCV_HOUGH_UTIL, &hough_gap_min, hough_gap_max);
 }
 
 Analyzer::~Analyzer() {
@@ -36,15 +32,66 @@ Analyzer::~Analyzer() {
 	cv::destroyWindow(OPENCV_HOUGH_UTIL);
 }
 
-void Analyzer::rotate(const cv::Mat& src, cv::Mat& dst) {
-	// Rotate and project the image
-	double r[3][3] = {
-			{ 0, -1, src.rows - 1 },
-			{ 1, 0, 0 },
-			{ 0, 0, 1 }
-	};
-	cv::Mat transform = cv::Mat(2, 3, CV_64F, r);
-	cv::warpAffine(src, dst, transform, cv::Size(src.rows, src.cols));
+void Analyzer::imageCb(const sensor_msgs::ImageConstPtr& msg) {
+	cv_bridge::CvImageConstPtr cv_ptr;
+	cv::Mat hdst;
+	cv::vector<cv::Vec4i> lines;
+	cv::Point best1, best2;
+
+	// Read the image from msg and store it in cv_ptr
+	try {
+		cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8);
+	} catch (cv_bridge::Exception& e) {
+		ROS_ERROR("cv_bridge exception: %s", e.what());
+		return;
+	}
+
+	// Process the image
+	rotate(cv_ptr->image, hdst);
+	project(hdst, hdst);
+	detect(hdst, hdst, lines);
+	filter(hdst, lines, hdst, best1, best2);
+	display(hdst);
+}
+
+void display(const cv::Mat& src) {
+	cv::Mat dst;
+	cv::resize(src, dst, cv::Size(src.cols / 2, src.rows / 2));
+	cv::imshow(OPENCV_HOUGH, src);
+}
+
+void Analyzer::detect(const cv::Mat& src, cv::Mat& dst, cv::vector<cv::Vec4i>& lines) {
+	// Do some preprocessing (GRAY -> CANNY EDGE)
+	cv::cvtColor(src, dst, CV_BGR2GRAY);
+	cv::Canny(dst, dst, canny_min, canny_min * canny_ratio, canny_kernel);
+
+	// Perform Hough line detection
+	cv::HoughLinesP(dst, lines, 1, CV_PI / 180, hough_min, hough_line_min, hough_gap_min);
+	cv::cvtColor(dst, dst, CV_GRAY2BGR);
+}
+
+void Analyzer::filter(	const cv::Mat& src, const cv::vector<cv::Vec4i>& lines,
+						cv::Mat& dst, cv::Point& best1, cv::Point& best2) {
+	cv::Point midbottom(dst.cols / 2, dst.rows);
+	int bestscore = INT_MIN;
+	for (size_t i = 0; i < lines.size(); i++) {
+		cv::Point p1(lines[i][0], lines[i][1]);
+		cv::Point p2(lines[i][2], lines[i][3]);
+
+		// -M_PI / 2 <= atan2 <= M_PI / 2, therefore we add M_PI when atan2 < 0 for a domain from 0 to M_PI
+		double angle = atan2(p2.y - p1.y, p2.x - p1.x);
+		if (angle < 0) angle += M_PI;
+		if (angle > 1 * M_PI / 6 && angle < 5 * M_PI / 6) {
+			cv::line(dst, p1, p2, cv::Scalar(0, 0, 255), 3);
+
+			double length = cv::norm(p1 - p2);
+			int dist = std::min(cv::norm(p1 - midbottom), cv::norm(p2 - midbottom));
+			int score = length / dist;
+
+			if (score > bestscore) { best1 = p1, best2 = p2; bestscore = score; }
+		}
+	}
+	cv::line(dst, best1, best2, cv::Scalar(255, 0, 0), 3);
 }
 
 void Analyzer::project(const cv::Mat& src, cv::Mat& dst) {
@@ -63,57 +110,15 @@ void Analyzer::project(const cv::Mat& src, cv::Mat& dst) {
 	cv::warpPerspective(src, src, transform, src.size());
 }
 
-void Analyzer::detect(const cv::Mat& src, cv::Mat& dst, cv::vector<cv::Vec4i>& lines) {
-	// Do some preprocessing (GRAY -> CANNY EDGE)
-	cv::cvtColor(src, dst, CV_BGR2GRAY);
-	cv::Canny(dst, dst, canny_min, canny_min * canny_ratio, canny_kernel);
-
-	// Perform Hough line detection
-	cv::HoughLinesP(dst, lines, 1, CV_PI / 180, hough_min, hough_line_min, hough_gap_min);
-	cv::cvtColor(dst, dst, CV_GRAY2BGR);
-}
-
-void Analyzer::imageCb(const sensor_msgs::ImageConstPtr& msg) {
-	cv_bridge::CvImageConstPtr cv_ptr;
-	cv::Mat hdst;
-	cv::vector<cv::Vec4i> lines;
-
-	// Read the image from msg and store it in cv_ptr
-	try {
-		cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8);
-	} catch (cv_bridge::Exception& e) {
-		ROS_ERROR("cv_bridge exception: %s", e.what());
-		return;
-	}
-
-	// Process the image
-	rotate(cv_ptr->image, hdst);
-	project(hdst, hdst);
-	detect(hdst, hdst, lines);
-
-	cv::Point best1, best2;
-	cv::Point midbottom(hdst.cols / 2, hdst.rows);
-	int bestscore = INT_MIN;
-	for (size_t i = 0; i < lines.size(); i++) {
-		cv::Point p1(lines[i][0], lines[i][1]);
-		cv::Point p2(lines[i][2], lines[i][3]);
-
-		// -M_PI / 2 <= atan2 <= M_PI / 2, therefore we add M_PI when atan2 < 0 for a domain from 0 to M_PI
-		double angle = atan2(p2.y - p1.y, p2.x - p1.x);
-		if (angle < 0) angle += M_PI;
-		if (angle > 1 * M_PI / 6 && angle < 5 * M_PI / 6) {
-			cv::line(hdst,	p1, p2, cv::Scalar(0, 0, 255), 3);
-
-			double length = cv::norm(p1 - p2);
-			int dist = std::min(cv::norm(p1 - midbottom), cv::norm(p2 - midbottom));
-			int score = length; // dist;
-			if (score > bestscore) { best1 = p1, best2 = p2; bestscore = score; }
-		}
-	}
-	cv::line(hdst, best1, best2, cv::Scalar(255, 0, 0), 3);
-	cv::resize(hdst, hdst, cv::Size(hdst.cols / 2, hdst.rows / 2));
-	cv::imshow(OPENCV_HOUGH, hdst);
-	cv::waitKey(3);
+void Analyzer::rotate(const cv::Mat& src, cv::Mat& dst) {
+	// Rotate and project the image
+	double r[3][3] = {
+			{ 0, -1, src.rows - 1 },
+			{ 1, 0, 0 },
+			{ 0, 0, 1 }
+	};
+	cv::Mat transform = cv::Mat(2, 3, CV_64F, r);
+	cv::warpAffine(src, dst, transform, cv::Size(src.rows, src.cols));
 }
 
 int main(int argc, char** argv) {
@@ -122,4 +127,3 @@ int main(int argc, char** argv) {
 	ros::spin();
 	return 0;
 }
-
--- linefollower/src/analyzer.h
+++ linefollower/src/analyzer.h
@@ -12,10 +12,14 @@ public:
   Analyzer();
   virtual ~Analyzer();
 
+  void imageCb(const sensor_msgs::ImageConstPtr& msg);
+
+  void display(const cv::Mat& src);
   void detect (const cv::Mat& src, cv::Mat& dst, cv::vector<cv::Vec4i>& lines);
+  void filter (const cv::Mat& src, const cv::vector<cv::Vec4i>& lines,
+               cv::Mat& dst, cv::Point& best1, cv::Point& best2);
   void project(const cv::Mat& src, cv::Mat& dst);
   void rotate (const cv::Mat& src, cv::Mat& dst);
-  void imageCb(const sensor_msgs::ImageConstPtr& msg);
 private:
   int canny_ratio;
   int canny_kernel;
